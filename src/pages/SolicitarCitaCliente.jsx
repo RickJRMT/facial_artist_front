506d6b1ae3178274c3e79b656828b6c1c510dcb8
Kesly Labio Otero <keslyotero@gmail.com>
2025-10-18 16:13:14 -0500

    se implementa validación de nacimiento para servicios de micropigmentación

--- src/pages/SolicitarCitaCliente.jsx
+++ src/pages/SolicitarCitaCliente.jsx
@@ -1,43 +1,41 @@
 // Importamos React y algunos hooks necesarios para manejar estado y efectos secundarios
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Importamos el archivo CSS para estilos específicos de esta página
-import './SolicitarCitaCliente.css';
+import "./SolicitarCitaCliente.css";
 
 // Componentes de cabecera y pie de página para mantener consistencia visual
-import Header from '../components/layout/Header_Cliente.jsx';
-import Footer from '../components/layout/Footer_Cliente.jsx';
+import Header from "../components/layout/Header_Cliente.jsx";
+import Footer from "../components/layout/Footer_Cliente.jsx";
 
 // Hook personalizado que maneja el resumen de la cita, para mostrar información actualizada
-import { useResumenCita } from '../hooks/Solicitar_Cita_Cliente.jsx';
+import { useResumenCita } from "../hooks/Solicitar_Cita_Cliente.jsx";
 
 // Componente que renderiza un calendario para seleccionar fechas
-import CalendarioCitas from '../components/layout/calendarioCitas.jsx';
+import CalendarioCitas from "../components/layout/calendarioCitas.jsx";
 // componente del modal que aparece tras solicitar una cita
-import ModalCitaExitosa from '../components/layout/ModalCitaSolicitada.jsx'
+import ModalCitaExitosa from "../components/layout/ModalCitaSolicitada.jsx";
 // Función que se conecta con el backend para crear una cita
-import { crearCita } from '../Services/citasClientesConexion';
+import { crearCita } from "../Services/citasClientesConexion";
 
 // Hooks personalizados que manejan la validación de formulario y carga de datos necesarios para la cita
-import { useValidacionFormulario } from '../hooks/ValidarFormCitaCliente.jsx';
-import { useProfesionales } from '../hooks/CargarProfesionales.jsx';
-import { UseServicios } from '../hooks/CargarServicios.jsx';
-import { useHorariosDisponibles } from '../hooks/CargarHorarios.jsx';
-import useModalCitaExitosa from '../hooks/useModalCitaExitosa';
-
-
-
+import { useValidacionFormulario } from "../hooks/ValidarFormCitaCliente.jsx";
+import { useProfesionales } from "../hooks/CargarProfesionales.jsx";
+import { UseServicios } from "../hooks/CargarServicios.jsx";
+import { useHorariosDisponibles } from "../hooks/CargarHorarios.jsx";
+import useModalCitaExitosa from "../hooks/useModalCitaExitosa";
 
 const SolicitarCitaPage = () => {
   // Hook para manejar los datos del formulario y su validación (nombre, teléfono, etc.)
-  const { formData, handleInputChange, limpiarFormulario } = useValidacionFormulario();
+  const { formData, handleInputChange, limpiarFormulario } =
+    useValidacionFormulario();
 
   // Hook para manejar y actualizar un resumen visible de la cita que se va configurando
   const { resumen, actualizarResumen } = useResumenCita();
 
   // Estado local para almacenar el profesional y servicio seleccionados (solo el id)
-  const [idProfesional, setIdProfesional] = useState('');
-  const [idServicio, setIdServicio] = useState('');
+  const [idProfesional, setIdProfesional] = useState("");
+  const [idServicio, setIdServicio] = useState("");
 
   // Obtenemos listas de profesionales, servicios y horarios disponibles (según selección)
   const { profesionales } = useProfesionales();
@@ -48,7 +46,8 @@ const SolicitarCitaPage = () => {
     resumen.fecha
   );
   // es para determinar el estado visible o no visible del modal:
-  const { modalVisible, mostrarModal, cerrarModal, datosCita } = useModalCitaExitosa();
+  const { modalVisible, mostrarModal, cerrarModal, datosCita } =
+    useModalCitaExitosa();
 
   // Función que se ejecuta cuando cambia la selección del profesional en el formulario
   // Actualiza el id en el estado local y también actualiza el resumen con el nombre del profesional
@@ -57,61 +56,109 @@ const SolicitarCitaPage = () => {
     setIdProfesional(id);
     // Buscamos el nombre del profesional seleccionado para actualizar el resumen
     // el fin busca el primer elemento de un array que cumpla con una condición especificada.
-    const nombre = profesionales.find((p) => p.idProfesional === parseInt(id))?.nombreProfesional || '';
-    actualizarResumen({ target: { name: 'profesional', value: nombre } });
+    const nombre =
+      profesionales.find((p) => p.idProfesional === parseInt(id))
+        ?.nombreProfesional || "";
+    actualizarResumen({ target: { name: "profesional", value: nombre } });
   };
-
 
   const handleServicioChange = (e) => {
     const id = e.target.value;
     setIdServicio(id);
     // Buscamos el nombre del servicio para actualizar el resumen
-    const servicioSeleccionado = servicios.find((s) => s.idServicios === parseInt(id));
-    const nombre = servicioSeleccionado?.servNombre || '';
-    const precio = servicioSeleccionado?.servCosto || 'No disponible';
+    const servicioSeleccionado = servicios.find(
+      (s) => s.idServicios === parseInt(id)
+    );
+    const nombre = servicioSeleccionado?.servNombre || "";
+    const precio = servicioSeleccionado?.servCosto || "No disponible";
 
     // Comprobamos si el precio no es igual a 'No disponible'
     // Si el precio es un valor válido, lo formateamos; si no, lo dejamos como 'No disponible'
-    const precioFormateado = precio !== 'No disponible' ?
-      // Si el precio está disponible, usamos Intl.NumberFormat para darle el formato adecuado
-      // 'es-ES' indica que usaremos el formato de número español, con puntos como separadores de miles.
-      // minimumFractionDigits: 2 asegura que siempre se muestren dos decimales.
-      new Intl.NumberFormat('es-ES', {
-        style: 'decimal',
-        minimumFractionDigits: (precio % 1 === 0) ? 0 : 2,  // Si es entero, no muestra decimales, si tiene decimales muestra 2
-        maximumFractionDigits: 2  // Limita el número de decimales a 2
-      }).format(precio)
-      : precio;  // Si el precio es "No disponible", lo dejamos tal cual
-    actualizarResumen({ target: { name: 'servicio', value: nombre } });
-    actualizarResumen({ target: { name: 'precio', value: precioFormateado } });
+    const precioFormateado =
+      precio !== "No disponible"
+        ? // Si el precio está disponible, usamos Intl.NumberFormat para darle el formato adecuado
+          // 'es-ES' indica que usaremos el formato de número español, con puntos como separadores de miles.
+          // minimumFractionDigits: 2 asegura que siempre se muestren dos decimales.
+          new Intl.NumberFormat("es-ES", {
+            style: "decimal",
+            minimumFractionDigits: precio % 1 === 0 ? 0 : 2, // Si es entero, no muestra decimales, si tiene decimales muestra 2
+            maximumFractionDigits: 2, // Limita el número de decimales a 2
+          }).format(precio)
+        : precio; // Si el precio es "No disponible", lo dejamos tal cual
+    actualizarResumen({ target: { name: "servicio", value: nombre } });
+    actualizarResumen({ target: { name: "precio", value: precioFormateado } });
   };
   // Función para limpiar todos los campos del formulario y resetear estados a valores iniciales
   const limpiarTodoFormulario = () => {
     limpiarFormulario(); // limpia los campos básicos (nombre, teléfono, etc.)
-    setIdProfesional(''); // resetea selección de profesional
-    setIdServicio(''); // resetea selección de servicio
+    setIdProfesional(""); // resetea selección de profesional
+    setIdServicio(""); // resetea selección de servicio
     // Resetea valores del resumen para que no quede información antigua visible
-    actualizarResumen({ target: { name: 'fecha', value: 'No seleccionada' } });
-    actualizarResumen({ target: { name: 'hora', value: 'No seleccionada' } });
-    actualizarResumen({ target: { name: 'servicio', value: 'Por seleccionar' } });
-    actualizarResumen({ target: { name: 'profesional', value: 'Por seleccionar' } });
+    actualizarResumen({ target: { name: "fecha", value: "No seleccionada" } });
+    actualizarResumen({ target: { name: "hora", value: "No seleccionada" } });
+    actualizarResumen({
+      target: { name: "servicio", value: "Por seleccionar" },
+    });
+    actualizarResumen({
+      target: { name: "profesional", value: "Por seleccionar" },
+    });
   };
+
+  // función para calcular la edad:
+  const calcularEdad = (fechaNacimiento) => {
+    const hoy = new Date();
+    const fechaNac = new Date(fechaNacimiento);
+    let edad = hoy.getFullYear() - fechaNac.getFullYear();
+    const mes = hoy.getMonth() - fechaNac.getMonth();
+
+    if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
+      edad--;
+    }
+
+    return edad;
+  };
+
   // Función que se ejecuta al enviar el formulario (al solicitar la cita)
   const manejarEnvio = async (e) => {
     e.preventDefault(); // Evita que el formulario recargue la página al enviar
     // Validamos que todos los campos obligatorios estén completos
 
     if (
-      resumen.fecha === 'No seleccionada' ||
-      resumen.hora === 'No seleccionada' ||
-      resumen.servicio === 'Por seleccionar' ||
-      resumen.profesional === 'Por seleccionar' ||
+      resumen.fecha === "No seleccionada" ||
+      resumen.hora === "No seleccionada" ||
+      resumen.servicio === "Por seleccionar" ||
+      resumen.profesional === "Por seleccionar" ||
       !idProfesional ||
       !idServicio
     ) {
-      alert('Por favor, completa todos los campos de la cita');
+      alert("Por favor, completa todos los campos de la cita");
       return; // No continúa si faltan campos
     }
+
+    const edadCliente = calcularEdad(formData.fechaNacCliente);
+
+    // Listado de servicios de micropigmentación
+    const serviciosProhibidos = [
+      "Micropigmentación de cejas",
+      "Micropigmentación de pestañas",
+    ];
+
+    // Verificar si el servicio seleccionado es uno de los prohibidos
+    const servicioSeleccionado = servicios.find(
+      (s) => s.idServicios === parseInt(idServicio)
+    )?.servNombre;
+
+    // Verificar si la edad es menor a 18 y el servicio es uno de los prohibidos
+    if (
+      edadCliente < 18 &&
+      serviciosProhibidos.includes(servicioSeleccionado)
+    ) {
+      alert(
+        "Lo siento, debes tener al menos 18 años para solicitar este servicio."
+      );
+      return; // No continuar si la validación falla
+    }
+
     // Creamos un objeto con todos los datos que se enviarán al backend para crear la cita
     const datosCita = {
       nombreCliente: formData.nombreCliente,
@@ -121,10 +168,11 @@ const SolicitarCitaPage = () => {
       idServicios: parseInt(idServicio),
       fechaCita: resumen.fecha,
       // Convertimos la hora seleccionada en formato 24h para el backend
-      horaCita: horariosDisponibles.find((h) => h.horaInicio === resumen.hora)?.horaInicio24,
+      horaCita: horariosDisponibles.find((h) => h.horaInicio === resumen.hora)
+        ?.horaInicio24,
       numeroReferencia: Math.floor(Date.now() / 1000),
     };
-
+    
     try {
       // Llamamos a la función que envía la cita al backend
       await crearCita(datosCita);
@@ -135,13 +183,13 @@ const SolicitarCitaPage = () => {
         profesional: resumen.profesional,
         servicio: resumen.servicio,
         costo: resumen.precio,
-        numeroReferencia: Math.floor(Date.now() / 1000)
+        numeroReferencia: Math.floor(Date.now() / 1000),
       });
       limpiarTodoFormulario();
     } catch (error) {
       // En caso de error mostramos un mensaje y lo registramos en consola para depuración
-      console.error('Error al enviar cita:', error);
-      alert(error.message || 'Error al solicitar cita');
+      console.error("Error al enviar cita:", error);
+      alert(error.message || "Error al solicitar cita");
     }
   };
 
@@ -151,7 +199,9 @@ const SolicitarCitaPage = () => {
       <div className="contenedor-principal">
         <div className="texto-centrado">
           <h1 className="titulo-principal">Solicitud de Citas</h1>
-          <p className="descripcion-principal">Selecciona tu fecha y horario a tu comodidad</p>
+          <p className="descripcion-principal">
+            Selecciona tu fecha y horario a tu comodidad
+          </p>
         </div>
         <div className="contenedor-citas">
           <div className="seccion-formulario">
@@ -197,10 +247,15 @@ const SolicitarCitaPage = () => {
                 value={idServicio}
                 required
               >
-                <option value="" disabled>Seleccione el tipo de servicio</option>
+                <option value="" disabled>
+                  Seleccione el tipo de servicio
+                </option>
                 {/* Renderizamos todas las opciones de servicios disponibles */}
                 {servicios.map((servicio) => (
-                  <option key={servicio.idServicios} value={servicio.idServicios}>
+                  <option
+                    key={servicio.idServicios}
+                    value={servicio.idServicios}
+                  >
                     {servicio.servNombre}
                   </option>
                 ))}
@@ -214,7 +269,9 @@ const SolicitarCitaPage = () => {
                 value={idProfesional}
                 required
               >
-                <option value="" disabled>Por seleccionar</option>
+                <option value="" disabled>
+                  Por seleccionar
+                </option>
                 {/* Renderizamos todas las opciones de profesionales disponibles */}
                 {profesionales.map((prof) => (
                   <option key={prof.idProfesional} value={prof.idProfesional}>
@@ -229,7 +286,7 @@ const SolicitarCitaPage = () => {
                 id="fecha"
                 name="fecha"
                 // Si no se ha seleccionado fecha, el input queda vacío
-                value={resumen.fecha !== 'No seleccionada' ? resumen.fecha : ''}
+                value={resumen.fecha !== "No seleccionada" ? resumen.fecha : ""}
                 onChange={actualizarResumen} // Actualiza el resumen con la fecha seleccionada
                 //   1. `new Date()` - Crea un objeto de fecha con la fecha y hora actual.
                 // 2. `toISOString()` - Convierte la fecha a una cadena en formato ISO 8601 (YYYY-MM-DDTHH:mm:ss.sssZ).
@@ -238,7 +295,7 @@ const SolicitarCitaPage = () => {
                 //    Ejemplo: ["2025-10-13", "14:30:00.000Z"]
                 // 4. `[0]` - Toma solo la primera parte del array (la fecha) para obtener solo el valor "2025-10-13".
                 // 5. `min={...}` - Establece la fecha mínima seleccionable en el campo de entrada como la fecha de hoy, evitando así que el usuario elija fechas pasadas.
-                min={new Date().toISOString().split('T')[0]} // Fecha mínima es hoy para evitar fechas pasadas
+                min={new Date().toISOString().split("T")[0]} // Fecha mínima es hoy para evitar fechas pasadas
                 required
               />
               {/* Selector de hora para la cita */}
@@ -250,7 +307,9 @@ const SolicitarCitaPage = () => {
                 value={resumen.hora}
                 required
               >
-                <option value="No seleccionada" disabled>Selecciona la hora</option>
+                <option value="No seleccionada" disabled>
+                  Selecciona la hora
+                </option>
                 {/* Renderizamos todas las horas disponibles para la selección */}
                 {horariosDisponibles.map((horario) => (
                   <option key={horario.horaInicio} value={horario.horaInicio}>
@@ -260,19 +319,24 @@ const SolicitarCitaPage = () => {
               </select>
 
               {/* Sección que muestra un resumen con los datos de la cita que se están configurando */}
-              <div className="resumen-cita" style={{ marginTop: '1rem' }}>
-                <strong>Resumen del agendamiento de cita:</strong><br />
+              <div className="resumen-cita" style={{ marginTop: "1rem" }}>
+                <strong>Resumen del agendamiento de cita:</strong>
                 <br />
-                Cliente: <span>{formData.nombreCliente || 'No ingresado'}</span><br />
-                Fecha: <span>{resumen.fecha || 'No seleccionada'}</span><br />
-                Hora: <span>{resumen.hora}</span><br />
-                Servicio: <span>{resumen.servicio}</span><br />
-                Precio: <span>{resumen.precio || 'No disponible'}</span> <br />
+                <br />
+                Cliente: <span>{formData.nombreCliente || "No ingresado"}</span>
+                <br />
+                Fecha: <span>{resumen.fecha || "No seleccionada"}</span>
+                <br />
+                Hora: <span>{resumen.hora}</span>
+                <br />
+                Servicio: <span>{resumen.servicio}</span>
+                <br />
+                Precio: <span>{resumen.precio || "No disponible"}</span> <br />
                 Profesional: <span>{resumen.profesional}</span>
               </div>
 
               {/* Botón para enviar la solicitud de cita */}
-              <button type="submit" style={{ marginTop: '1rem' }}>
+              <button type="submit" style={{ marginTop: "1rem" }}>
                 Solicitar Cita
               </button>
             </form>
@@ -289,7 +353,7 @@ const SolicitarCitaPage = () => {
       {modalVisible && (
         <ModalCitaExitosa
           datosCita={datosCita}
-          onClose={cerrarModal}  // Cerramos el modal al hacer clic en "Cerrar"
+          onClose={cerrarModal} // Cerramos el modal al hacer clic en "Cerrar"
         />
       )}
     </>
